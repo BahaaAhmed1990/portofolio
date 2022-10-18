c714efdba5d12a7fa2c9ce1eb0f53831b6fcb378
Bahaa Ahmed <bahaaahmed19902012@gmail.com>
2022-10-18 23:34:44 +0200

    add about me skeleton

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,10 @@
 import home from "../js/app/app-home.js";
 import menu from "../js/app/app-menu.js";
 (function () {
+  window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+    console.log("first");
+  };
   //======== start menu
   if ($(".menu")) {
     menu.init();
@@ -13,4 +17,17 @@ import menu from "../js/app/app-menu.js";
 
     home.initSectionHome();
   }
+  function reveal() {
+    const sections = $(".section");
+    for (let i = 0; i < sections.length; i++) {
+      const elementTop = sections[i].getBoundingClientRect().top;
+      const windowHeight = window.innerHeight;
+      const elementVisible = 150;
+      if (elementVisible < windowHeight - elementTop) {
+        sections[i].classList.add("reveal");
+      }
+    }
+  }
+  window.addEventListener("scroll", reveal);
+  reveal();
 })();
